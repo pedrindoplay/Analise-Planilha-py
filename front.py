ec1e428745b979a5f6b7d1c7e5d04077d271031c
pedrindoplay <manjaguilherme21@gmail.com>
2026-05-21 10:50:52 -0300

    mexi em pequenos detalhes

--- front.py
+++ front.py
@@ -1,19 +1,21 @@
 from analise import pesquisa, todos, analisar
 
+paginas = int(input("Quantas pagians possui o arquivo?:  "))
+
 while True:
     menu = int(input("Oque você quer fazer?\nAnalisar a planilha?(1)\nVer todos os livros?(2)\nPesquisar um livro?(3)\nsair(4)\n"))
     if menu == 1:
-        print(analisar(int(input("\nQual página você quer analisar?\n"))))
-    
-    
+        a = int(input("Qual pagina você quer ver?  "))
+        deixados, levados = analisar(a)
+        print(f"\nOs livros deixados são:\n{deixados}\n\nOs livros que foram levados são\n{levados}\n\n")
+
+
     elif menu == 2:
-        paginas = int(input("\nQuantas páginas tem na planilha?  "))
         deixados, levados, todos = todos(paginas)
-        print(f"\nOs livros deixados são:\n{deixados}\nOs livros que foram levados são\n{levados}")
+        print(f"\nOs livros deixados são:\n{deixados}\n\nOs livros que foram levados são\n{levados}")
     
     
     elif menu == 3:
-        paginas = int(input("quantas paginas tem o arquivo?:  "))
         livro = input("\nQual livro você quer procurar?:  ")
         print(pesquisa(todos(paginas)[2], livro))
     
